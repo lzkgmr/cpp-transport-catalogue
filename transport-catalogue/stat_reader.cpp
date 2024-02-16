bdff425a05f71b6f21b0392425ec4486f18320b4
lzkgmr <91319276+lzkgmr@users.noreply.github.com>
2024-02-16 16:34:50 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,1 +1,43 @@
-// место для вашего кода
+#include "stat_reader.h"
+// не могу придумать, как должны называться пространства имен и где они должны быть. дайте совет пожалуйста
+void BusInfo(const TransportCatalogue& transport_catalogue, std::string_view request,
+         std::ostream& output) {
+    Bus bus = transport_catalogue.FindBus(request);
+    if (bus.name.empty()) {
+        output << "Bus " << request << ": not found" << std::endl;
+    } else {
+        output << "Bus " << request << ": " << bus.stops.size() << " stops on route, " << bus.unique_stops << " unique stops, "  << bus.route_length << " route length" << std::endl;
+    }
+}
+
+void StopInfo(const TransportCatalogue& transport_catalogue, std::string_view request,
+              std::ostream& output) {
+    if (!transport_catalogue.FindStop(request)) {
+        output << "Stop " << request << ": not found" << std::endl;
+        return;
+    }
+    std::set<std::string> buses;
+    transport_catalogue.FindStopInfo(request, buses);
+    if (buses.empty()) {
+        output << "Stop " << request << ": no buses" << std::endl;
+    } else {
+        output << "Stop " << request << ": buses ";
+        auto it = buses.begin();
+        while (it != buses.end()) {
+            output << *it;
+            if (++it != buses.end()) output << " ";
+            else output << std::endl;
+        }
+    }
+}
+
+void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request,
+                       std::ostream& output) {
+    std::string_view request_str(request.substr(request.find_first_of(" ") + 1));
+    if (request[0] == 'B') {
+        BusInfo(transport_catalogue, request_str, output);
+    } else {
+        StopInfo(transport_catalogue, request_str, output);
+    };
+
+}
