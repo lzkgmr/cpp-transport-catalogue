bf1b11a1c5281bdaa6365675bd3b7b8d1ff7dc68
lzkgmr <91319276+lzkgmr@users.noreply.github.com>
2024-02-16 20:23:10 +0300

    2v

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <cmath>
-// не могу придумать, как должны называться пространства имен и где они должны быть. дайте совет пожалуйста
+
+const double EARTH_RADIUS = 6371000;
+
 struct Coordinates {
     double lat;
     double lng;
@@ -21,5 +23,5 @@ inline double ComputeDistance(Coordinates from, Coordinates to) {
     static const double dr = 3.1415926535 / 180.;
     return acos(sin(from.lat * dr) * sin(to.lat * dr)
                 + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * 6371000;
+        * EARTH_RADIUS;
 }
