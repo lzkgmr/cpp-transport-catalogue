d40b2131c5847109ba906f842d14c040fb75fa77
lzkgmr <91319276+lzkgmr@users.noreply.github.com>
2024-02-16 16:33:23 +0300

    first ver

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,15 @@
-// место для вашего кода
+#pragma once
+
+#include <iosfwd>
+#include <string_view>
+
+#include "transport_catalogue.h"
+// не могу придумать, как должны называться пространства имен и где они должны быть. дайте совет пожалуйста
+void BusInfo(const TransportCatalogue& transport_catalogue, std::string_view request,
+             std::ostream& output);
+
+void StopInfo(const TransportCatalogue& transport_catalogue, std::string_view request,
+              std::ostream& output);
+
+void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request,
+                       std::ostream& output);
