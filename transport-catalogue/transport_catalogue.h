29a7716ec8ab0cfd3a667e3ada6d1fe3af79b6dd
lzkgmr <91319276+lzkgmr@users.noreply.github.com>
2024-02-16 16:34:09 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,111 @@
-// место для вашего кода
+#pragma once
+// не могу придумать, как должны называться пространства имен и где они должны быть. дайте совет пожалуйста
+#include <deque>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <string>
+#include <functional>
+#include <string>
+#include <iostream>
+#include <set>
+#include <stdexcept>
+
+#include "geo.h"
+
+struct Bus;
+
+struct Stop {
+    std::string name;
+    Coordinates cordinates;
+};
+
+struct Bus {
+    std::string name;
+    std::vector<const Stop*> stops;
+    int unique_stops = 0;
+    double route_length = 0;
+};
+
+class TransportCatalogue {
+public:
+    void AddStop(std::string name, Coordinates coordinates);
+
+    bool FindStop(std::string_view name) const;
+
+    void FindStopInfo(std::string_view name, std::set<std::string>& buses) const;
+
+    void AddBus(std::string name, std::vector <std::string_view> stops);
+
+    Bus FindBus(std::string_view name) const;
+
+private:
+    class StrHasher {
+    public:
+        size_t operator()(const std::string_view& name) const;
+    private:
+        std::hash<std::string> hasher_;
+    };
+
+
+    std::deque<Stop> stops_;
+    std::deque<Bus> buses_;
+    std::unordered_map<std::string_view, const Stop*, StrHasher> stopname_to_stop;
+    std::unordered_map<std::string_view, const Bus*, StrHasher> busname_to_bus;
+};#pragma once
+// не могу придумать, как должны называться пространства имен и где они должны быть. дайте совет пожалуйста
+#include <deque>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <string>
+#include <functional>
+#include <string>
+#include <iostream>
+#include <set>
+#include <stdexcept>
+
+#include "geo.h"
+
+struct Bus;
+
+struct Stop {
+    std::string name;
+    Coordinates cordinates;
+};
+
+struct Bus {
+    std::string name;
+    std::vector<const Stop*> stops;
+    int unique_stops = 0;
+    double route_length = 0;
+};
+
+class TransportCatalogue {
+public:
+    void AddStop(std::string name, Coordinates coordinates);
+
+    bool FindStop(std::string_view name) const;
+
+    void FindStopInfo(std::string_view name, std::set<std::string>& buses) const;
+
+    void AddBus(std::string name, std::vector <std::string_view> stops);
+
+    Bus FindBus(std::string_view name) const;
+
+private:
+    class StrHasher {
+    public:
+        size_t operator()(const std::string_view& name) const;
+    private:
+        std::hash<std::string> hasher_;
+    };
+
+
+    std::deque<Stop> stops_;
+    std::deque<Bus> buses_;
+    std::unordered_map<std::string_view, const Stop*, StrHasher> stopname_to_stop;
+    std::unordered_map<std::string_view, const Bus*, StrHasher> busname_to_bus;
+};
